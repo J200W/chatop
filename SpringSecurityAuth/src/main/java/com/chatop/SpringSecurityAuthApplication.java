60a1c1cb9e5813a34e6f0ad606a4212c2fd32de5
J200W <94389835+J200W@users.noreply.github.com>
2024-03-10 14:49:50 +0100

    Revert "data.sql initialized"
    This reverts commit 22f525420a361aa1df90bf281840a60b4d9b9cb5.

--- SpringSecurityAuth/src/main/java/com/chatop/SpringSecurityAuthApplication.java
+++ SpringSecurityAuth/src/main/java/com/chatop/SpringSecurityAuthApplication.java
@@ -1,13 +1,13 @@
-package com.chatop.api;
+package com.chatop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class SpringSecurityAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(SpringSecurityAuthApplication.class, args);
 	}
 
 }
--- SpringSecurityAuth/src/test/java/com/chatop/SpringSecurityAuthApplicationTests.java
+++ SpringSecurityAuth/src/test/java/com/chatop/SpringSecurityAuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.chatop.api;
+package com.chatop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class SpringSecurityAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
